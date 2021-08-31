8edb5b845fa6eb84275e5a00781c423b450e3c32
Bongjungi <kokkohkof@naver.com>
2021-08-31 10:06:31 +0900

    before bootstrap

--- blog/views.py
+++ blog/views.py
@@ -6,12 +6,15 @@ class PostList(ListView):
     model = Post
     def get_queryset(self):
         return Post.objects.order_by('-created')
+
+class PostDetail(DetailView):
+    model = Post
+
 # def index(request):    #위의 postlist가 자동으로 이렇게 됨
 #     object_list = Post.objects.all()
 #     context = {'object_list':object_list}
 #     return render(request, 'blog/post_list.html', context)
-
-
-def detail(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(request, 'blog/detail.html', {'post':post})
+# def detail(request, pk):
+#     post = Post.objects.get(pk=pk)
+    # context = {'object':post, 'post':post}
+#     return render(request, 'blog/post_detail.html', context)
