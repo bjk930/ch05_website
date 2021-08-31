6ce89ef578f17df0dea27a6e22b53e0fd12cb5c4
Bongjungi <kokkohkof@naver.com>
2021-08-31 09:56:21 +0900

    api

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
-def index(request):
-    posts = Post.objects.all()
-    context = {'posts':posts}
-    return render(request, 'blog/index.html', context)
+class PostList(ListView):
+    model = Post
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+# def index(request):    #위의 postlist가 자동으로 이렇게 됨
+#     object_list = Post.objects.all()
+#     context = {'object_list':object_list}
+#     return render(request, 'blog/post_list.html', context)
+
+
 def detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/detail.html', {'post':post})
